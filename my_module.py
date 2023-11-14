796bd2f86202bcf6599b38b26da6728c2a2e39f4
Nazar Gentchev <nazar.gentchev@mail.ru>
2023-11-14 14:02:07 +0200

    Ваш комментарий к коммиту

---
+++ my_module.py
@@ -0,0 +1,38 @@
+# my_module.py
+
+def sum2numbers(num1,num2):
+    """Summ of 2 numbers
+
+    Args:
+        num1 (float) or (int): first number.
+        num2 (float) or (int): second number.
+
+    Returns:
+        float or int: Summ.
+    """
+    summ = num1 + num2
+    return summ
+
+def welcom_user(name):
+    """Greet the user.
+
+    Args:
+        name (str): Name of the user.
+
+    Returns:
+        str: greeting message.
+    """
+    greeting = f"Hello, {name}!"
+    return greeting
+
+def split_string_by_comma(input_string):
+    """Split a string into a list using commas as separators.
+
+    Args:
+        input_string (str): The input string to be split.
+
+    Returns:
+        list: A list of substrings separated by commas.
+    """
+    result = input_string.split(',')
+    return result
