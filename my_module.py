0a2715fd7b5ba0b634bc64d8d6dbb19fc6fb83b4
Nazar Gentchev <nazar.gentchev@mail.ru>
2023-11-14 14:49:48 +0200

    Ваш комментарий к коммиту

--- my_module.py
+++ my_module.py
@@ -25,3 +25,15 @@ def split_string_by_comma(input_string):
     """
     result = input_string.split(',')
     return result
+def write_to_file(file_path, content):
+    """Write a string to a file.
+
+    Args:
+        file_path (str): The path to the file where the content will be written.
+        content (str): The string to be written to the file.
+
+    Returns:
+        None
+    """
+    with open(file_path, 'w') as file:
+        file.write(content)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from my_module import sum2numbers, split_string_by_comma
+from my_module import sum2numbers, split_string_by_comma, write_to_file
 
 class Test(unittest.TestCase):
     def test_function1(self):
@@ -10,3 +10,7 @@ class Test(unittest.TestCase):
     def test_function2(self):
         result = split_string_by_comma("Helow,dog,bag")  # Замените на ваши реальные аргументы
         self.assertEqual(result, ["Helow", "dog", "bag"])
+
+    def test_function3(self):
+        result = write_to_file("file.txt","Helow Nazar")  # Замените на ваши реальные аргументы
+        self.assertTrue(result)
